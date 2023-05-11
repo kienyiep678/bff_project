498f7eba8678ece01bd730d3875392163fb66876
kien yiep <kienyiep@gmail.com>
2023-05-11 14:47:25 +0800

    latest

--- ms-task-planner-sdk/src/main/java/dc/icdc/mstaskplannersdk/service/CreateTaskSdk.java
+++ ms-task-planner-sdk/src/main/java/dc/icdc/mstaskplannersdk/service/CreateTaskSdk.java
@@ -3,10 +3,13 @@ package dc.icdc.mstaskplannersdk.service;
 import dc.icdc.lib.common.model.dto.CommonResponse;
 import dc.icdc.lib.hi.helper.HostIntegrationHelper;
 import dc.icdc.lib.hi.model.dto.HostIntegrationDTO;
-import dc.icdc.mstaskplannermodel.dto.GetProjectDetailsResponse;
-import dc.icdc.mstaskplannermodel.dto.SearchProjectRequest;
+//import dc.icdc.mstaskplannermodel.dto.GetProjectDetailsResponse;
+import dc.icdc.mstaskplannermodel.dto.ProjectLessDTO;
+import dc.icdc.mstaskplannermodel.dto.SearchProjectDTO;
+//import dc.icdc.mstaskplannermodel.dto.SearchProjectRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.RequestBody;
 
 public class CreateTaskSdk {
@@ -14,14 +17,14 @@ public class CreateTaskSdk {
     @Autowired
     private HostIntegrationHelper hostIntegrationHelper;
 
-    public CommonResponse<SearchProjectRequest> createNewTechnicalProfile(@RequestBody(required = false) SearchProjectRequest request){
-        HostIntegrationDTO<SearchProjectRequest, CommonResponse<SearchProjectRequest>> integrationDTO =
-                HostIntegrationDTO.<SearchProjectRequest,  CommonResponse<SearchProjectRequest>> builder()
-                        .serviceName("search-task-planner")
-                        .reference(new ParameterizedTypeReference<CommonResponse<SearchProjectRequest>>(){})
-                        .requestBody(request)
-                        .build();
 
+    public CommonResponse<Page<ProjectLessDTO>> findAllProject(@RequestBody SearchProjectDTO request) {
+        HostIntegrationDTO<SearchProjectDTO, CommonResponse<Page<ProjectLessDTO>>> integrationDTO =
+                HostIntegrationDTO.<SearchProjectDTO, CommonResponse<Page<ProjectLessDTO>>>builder()
+                        .serviceName("search-task-planner")
+                        .requestBody(request)
+                        .outputParameterizedTypeReference(new ParameterizedTypeReference<CommonResponse<Page<ProjectLessDTO>>>() {})
+                        .build();
         return hostIntegrationHelper.integrate(integrationDTO);
     }
 
